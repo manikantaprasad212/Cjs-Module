78660403ffe0f87afcd0d88058b6bdea9ac90c3e
manikantaprasad212 <manikantaprasadprasadula@gmail.com>
2024-09-02 10:27:13 +0530

    Add files via upload

---
+++ Cjs Module/boat.js
@@ -0,0 +1,8 @@
+let brand = "BOAT";
+let mainProdut = "Earphones";
+let productCategories = ["Earphones","Earbuds","Headphones","Speakers","Smart Watches","Fitness Brands","Home audio system"]
+let boatBrandValue = 1400000000;
+let aboutboat = ()=>{
+    console.log("boAt has carved out a niche for itself in the highly competitive audio and electronics market,becoming a go-to brand for budget-conscious consumers looking for trendy and reliable products.Affordable Pricing: boAt products are priced to be accessible without compromising on quality.Trendy Designs: Focuses on modern, stylish designs that appeal to a younger demographic.Durability: Products are designed to withstand rough use, especially with features like water and sweat resistance.Innovation: Continuously expanding its product line to include the latest tech and design trends.")
+};
+module.exports ={brand,mainProdut,productCategories,boatBrandValue,aboutboat};
---
+++ Cjs Module/server.js
@@ -0,0 +1,11 @@
+let {brand,mainProdut,productCategories,boatBrandValue,aboutboat} = require("./boat");
+
+// console.log(boatBrand.brand);
+// console.log(boatBrand.mainProdut);
+// console.log(boatBrand.productCategories);
+// boatBrand.aboutboat();
+console.log(brand);
+console.log(mainProdut);
+console.log(productCategories);
+aboutboat();
+console.log(boatBrandValue);
